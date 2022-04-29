8cb2923d1441498a3f8e08186a732f87c28b0408
Corey Sklenar <sklenarcorey@gmail.com>
2022-04-29 13:38:47 -0500

    my first commit

---
+++ level-1/dj-js/app.js
@@ -0,0 +1,14 @@
+var hover = document.getElementById("hover")
+hover.addEventListener("mouseover", function() {
+    console.log("mouseover")
+    hover.style.backgroundColor = "blue";
+    })
+
+    addEventListener("mousedown", function() {
+        console.log("mousedown");
+        hover.style.backgroundColor = "red";
+ })
+    
+ addEventListener("mouseleave", function() {
+     console.log("mouseleave")
+ })
