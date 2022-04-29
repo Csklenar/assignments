ffd8b699a77add1e98c2bce5db1444bb1945b9a7
Corey Sklenar <sklenarcorey@gmail.com>
2022-04-29 13:46:34 -0500

    added a comment to dj-js

--- level-1/dj-js/app.js
+++ level-1/dj-js/app.js
@@ -1,14 +1,16 @@
 var hover = document.getElementById("hover")
-hover.addEventListener("mouseover", function() {
+hover.addEventListener("mouseover", function () {
     console.log("mouseover")
     hover.style.backgroundColor = "blue";
-    })
+})
 
-    addEventListener("mousedown", function() {
-        console.log("mousedown");
-        hover.style.backgroundColor = "red";
- })
-    
- addEventListener("mouseleave", function() {
-     console.log("mouseleave")
- })
+addEventListener("mousedown", function () {
+    console.log("mousedown");
+    hover.style.backgroundColor = "red";
+})
+
+addEventListener("mouseleave", function () {
+    console.log("mouseleave")
+})
+
+ // anything
