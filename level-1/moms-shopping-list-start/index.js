98ee10d32744f6c77220819885e887b537b9a75c
Corey Sklenar <sklenarcorey@gmail.com>
2022-05-04 14:24:38 -0500

    finished working on moms-shopping-list assignment

--- level-1/moms-shopping-list-start/index.js
+++ level-1/moms-shopping-list-start/index.js
@@ -2,13 +2,42 @@ const form = document["addItem"]
 form.addEventListener("submit", function (event) {
     event.preventDefault()
 
-    const firstItem = form.title.value
+    const items = form.title.value
     form.title.value = "";
-    console.log(firstItem);
+    console.log(items);
 
     const li = document.createElement("li");
-    li.textContent = firstItem;
+    li.textContent = items;
     document.getElementById("list").append(li);
 
     const button = document.createElement("button");
-});
+    document.getElementById("list").append(button)
+    button.textContent = "x"
+
+    button.addEventListener("click", function () {
+        console.log("Item removed");
+
+        var removeItem = document.getElementById("list")
+        removeItem.remove();
+        console.log(removeItem)
+    })
+
+    const editButton = document.createElement("button")
+    document.getElementById("list").append(editButton)
+    editButton.textContent = "edit"
+
+    editButton.addEventListener("click", function () {
+        editButton.textContent = "save";
+        const input = document.createElement("input")
+        li.append(input);
+
+        input.value = items;
+
+        editButton.textContent.remove();
+    })
+
+    if (editButton.textContent = "save") {
+        editButton.textContent = "edit"
+    }
+}
+); 
