3b6c1480065a3c8e67256a71eb08dbb601396de6
Corey Sklenar <sklenarcorey@gmail.com>
2022-05-23 13:32:36 -0500

    added folders of exercises that were recently complete, created a script.js file in the problem solving exercise and wrote funtions that returned certain words and numbers.

---
+++ level-1/fizzbuzz-exercise/script.js
@@ -0,0 +1,22 @@
+for (let i = 0; i <= 100; i++) {
+    if (i % 15 == 0 ) {
+        console.log("FizzBuzz")
+    } else if( i % 3 == 0) {
+            console.log("fizz")
+        } else if ( i % 5 == 0) {
+            console.log("Buzz")
+        } else {
+            console.log(i)
+        }
+    }
+    function tally() {
+        
+    }
+
+
+// for (var i = 0; i < 101; i++){
+//     if(i % 15 == 0) console.log("fizzbuzz");
+//     else if(i % 3== 0) console.log("fizz");
+//     else if(i % 5 == 0) console.log("buzz");
+//     else(console.log(i))
+// }
---
+++ level-1/problem-solving-practice/script.js
@@ -0,0 +1,52 @@
+// let arr = [1, 3, 6, 8, 9, 12, 15, 17, 20]
+
+
+// function returnLargestNum(arr) {
+//     let largestNum = 0;
+    
+
+//     for (let i = 0; i < arr.length; i += 1) {
+//         if (arr[i] > largestNum) {
+//             largestNum = arr[i]
+//         }
+//     }
+//     return largestNum;
+// }
+
+// console.log(returnLargestNum(arr));
+
+// let arrayOfWords = ["c&t", "dog", "Mo*use&", "Fish$&"]
+
+// function wordsWithStrings(array, letter) {
+  
+
+//     for (let i = 0; i < array.length; i++) {
+//         for (let j = 0; j < array[i].length; j++) {
+//             if (array[i][j] === letter) {
+//                 console.log(array[i])
+//             }
+//         }
+//     }
+// }
+// wordsWithStrings(arrayOfWords, "&")
+
+let num1 = 4;
+console.log(num1)
+
+let num2 = 2;
+console.log(num2)
+
+function isDivisible(num1, num2) {
+    if(num1 % num2 === 0) {
+        console.log("true");
+    } else {
+       console.log("false");
+    }
+}
+isDivisible(num1, num2);
+
+    
+
+  
+
+
